d89fe2d355885838fa653611e0fd67343eeb11be
Jorge García <sirsykon@gmail.com>
2021-11-11 16:03:18 +0100

    Code refactorized in order to use rosbag, rospy and ros_numpy instead of bagpy.

--- lidar_processing_utils/lidar_processing_utils/lidar_decoder.py
+++ lidar_processing_utils/lidar_processing_utils/lidar_decoder.py
@@ -1,13 +1,19 @@
 """
 Code to process LIDAR information.
 Author: Jorge García-González
-Last Update: 8/11/2021
+Last Update: 11/11/2021
 """
 
+import time
 from bagpy import bagreader
 import pandas as pd
 import os
 import numpy as np
+import open3d as o3d
+import rosbag
+import rospy
+import ros_numpy
+from typing import Tuple, List
 
 def get_cloud_point_from_bag(bag_path:str) -> np.ndarray:
     """
@@ -38,24 +44,40 @@ def read_cloud_csv(cloud_csv_path:str, print_info:bool=False) -> pd.DataFrame:
     print_info : bool -> Do we print dataframe information?
     """
 
+    print(f"Reading .csv from {os.path.abspath(cloud_csv_path)}")
     df = pd.read_csv(cloud_csv_path)
-    df['data'] = df['data'].apply(lambda d : np.frombuffer(d[2:-1].encode('latin-1').decode('unicode-escape').encode('latin-1'), np.uint8))
+    df['data'] = df['data'].apply(lambda d : np.frombuffer(d[2:-1].encode('latin-1').decode('unicode-escape').encode('latin-1'), np.float32))
 
     if print_info:
         print("COLUMNS")
         print(df.columns)
         print(df.data)
-        
-def from_pointcloud2_format_to_numpy(pointcloud2_vector:np.ndarray, row_length:int) -> np.ndarray:
+
+    return df
+
+def get_points_from_lidar_msg(msg:pd.Series) -> np.ndarray:
     """
-    Function to transform a vector containing pointcloud2 array information to a 2D-matrix.
-    pointcloud2_vector : list -> pointcloud2 vector.
-    row_length : int -> Length to split pointcloud2_vector.
+    Function to get a Nx3 matrix with the N points positions in msg.
+    msg : pd.Series -> Lidar message.
     """
-    print(pointcloud2_vector)
-    print(row_length)
+
+    pointcloud2_vector = msg['data']
     number_of_points = int(pointcloud2_vector.shape[0]/4)
-    print(number_of_points)
+    pointcloud2_matrix = pointcloud2_vector.reshape((number_of_points,4))
+
+    return pointcloud2_matrix[:,:3]
+
+def from_lidar_msg_to_numpy(msg:pd.Series) -> np.ndarray:
+    """
+    Function to transform a msg containing pointcloud2 array information to a 3D-matrix.
+    msg : pd.Series -> Lidar message.
+    """
+    print(msg)
+    pointcloud2_vector = msg['data']
+    row_length = msg['row_step']
+    print(pointcloud2_vector)
+    number_of_points = int(pointcloud2_vector.shape[0]/4)
+    print(f"Number of points: {number_of_points}")
     pointcloud2_matrix = pointcloud2_vector.reshape((number_of_points,4))
     print(pointcloud2_matrix.shape)
     max_x, max_y, max_z, max_intensity = list(np.max(pointcloud2_matrix, axis=0))
@@ -63,6 +85,117 @@ def from_pointcloud2_format_to_numpy(pointcloud2_vector:np.ndarray, row_length:i
     print(max_y)
     print(max_z)
     print(max_intensity)
-    print(pointcloud2_matrix)
+    points_matrix = np.zeros(shape=(max_x+1, max_y+1, max_z+1), dtype=np.uint8)
+    for point_cloud2 in pointcloud2_matrix:
+        points_matrix[point_cloud2[0], point_cloud2[1], point_cloud2[2]] = point_cloud2[3]
+    print(points_matrix)
+
+    return points_matrix
+
+def project_to_2D(points_matrix:np.ndarray, axis:int) -> np.ndarray:
+
+    assert len(points_matrix) > axis
+
+    projected_points_matrix = np.sum(points_matrix, axis=axis)
+    normalized_projected_points_matrix = projected_points_matrix
+
+    return normalized_projected_points_matrix/np.max(normalized_projected_points_matrix)
+
+def generator_msgs_from_rosbag(bag:rosbag.bag.Bag, topics:List[str]) -> Tuple[str, object, rospy.rostime.Time]:
+    """
+    Function to return ros messages contained in a ros .bag file in bag_path.
+    bag:rosbag.bag.Bag -> The bag to read messages from.
+    topics:list(str) -> Topics to get messages from the bag.
+    """
+    for topic, msg, t in bag.read_messages(topics=topics):
+        yield topic, msg, t
+ 
+
+def read_bag(bag_path:str, print_bag_info:bool=False) -> rosbag.bag.Bag:
+    """
+    Function to get a rosbag bag from ros .bag file.
+    bag_path:str -> Path to ros bag file.
+    print_bag_info:bool -> Do we print bag info? Default: False.
+    """
+    bag = rosbag.Bag(bag_path)
+
+    if print:
+        print(f"Bag topics: {bag.get_type_and_topic_info()[1].keys()}")
+
+    return bag
+
+
+def close_bag(bag:rosbag.bag.Bag):
+    bag.close()
+
+def get_numpy_sparse_representation_from_pointcloud2_msg(pointcloud2_msg:object):
+    """
+    Funtion to get a numpy matrix with the sparse representation of points contained in a point cloud 2 ros message.
+    pointcloud2_msg:object -> The point cloud 2 ros message to get the points from.
+    """
+    sparse_representation_of_points = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pointcloud2_msg, remove_nans=True) 
+    return sparse_representation_of_points
+
+def show_cloud(sparse_representation_of_points:np.ndarray) -> None:
+    """
+    Procedure to show 3D vidualization of sparse representation of points.
+    sparse_representation_of_points : np.ndarray -> Nx3 matrix with N (x,y,z) points.
+    """
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(sparse_representation_of_points)
+    o3d.visualization.draw_geometries([pcd])
+
+def initialize_cloud_rendering(sparse_representation_of_points:np.ndarray) -> o3d.cpu.pybind.visualization.Visualizer:
+    """
+    TODO: Description.
+    """
+
+    vis = o3d.visualization.Visualizer()
+    print(type(vis))
+    vis.create_window()
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(sparse_representation_of_points)
+    vis.add_geometry(pcd)
+    return vis, pcd
+
+def update_cloud_rendering(vis:o3d.cpu.pybind.visualization.Visualizer, pcd, sparse_representation_of_points:np.ndarray) -> o3d.cpu.pybind.visualization.Visualizer:
+    """
+    TODO: Description.
+    """
+
+    pcd.points = o3d.utility.Vector3dVector(sparse_representation_of_points)
+    vis.update_geometry(pcd)
+    vis.poll_events()
+    vis.update_renderer()
+
+    return vis, pcd
+
+def show_cloud_messages_from_rosbag(rosbag:rosbag.bag.Bag) -> None:
+    """
+    Process to get all cloud messages from a bag and visualize them in 3D real time.
+    rosbag : rosbag.bag.Bag -> Ros bag as readed by rosbag package.
+    """
+
+    print("Rendering point clouds from received bag.")
+    vis = None
+    dt = 0.1
+    last_time = None
+    for topic, msg, t in generator_msgs_from_rosbag(rosbag, ['/cloud']):
+        sparse_representation = get_numpy_sparse_representation_from_pointcloud2_msg(msg)
+        if last_time is None:
+            last_time = t.to_time()
+        else:
+            aux_t = t.to_time()
+            dt = aux_t - last_time
+            last_time = aux_t
+        time.sleep(dt)
+        if vis is None:
+            vis, pcd = initialize_cloud_rendering(sparse_representation)
+        else:
+            vis, pcd = update_cloud_rendering(vis,pcd,sparse_representation)
+
+    print("Redering finished.")
+
 
 
--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import pandas as pd
-import os
-import numpy as np
-import sys
 
+import time
 import lidar_processing_utils.lidar_processing_utils as lidar_processing_utils
 
-example_bag_path = "./input/prueba2.bag"
+example_bag_path = "./input/20211105_Aurora_ZirkerSee_9.bag"
+topics = ['/cloud']
 
-csv_path = lidar_processing_utils.generate_csv_from_bag(example_bag_path, "/cloud")
+example_bag = lidar_processing_utils.read_bag(example_bag_path)
 
-lidar_processing_utils.read_cloud_csv(csv_path, print_info=True)
+lidar_processing_utils.show_cloud_messages_from_rosbag(example_bag)
 
-
-df = pd.read_csv(csv_path)
-print(df.columns)
-t = df.data[210]
-t1 = t[2:10].encode('latin-1').decode('unicode-escape')
-print("t1")
-print(type(t1))
-print(t1)
-print(t1.encode('latin-1'))
-print(np.frombuffer(t1.encode("latin-1"), np.uint8))
-
-df['data'] = df['data'].apply(lambda d : np.frombuffer(d[2:-1].encode('latin-1').decode('unicode-escape').encode('latin-1'), np.uint8))
-print(len(df['data'][200]))
-
-lidar_processing_utils.from_pointcloud2_format_to_numpy(df['data'][200], df['row_step'][200])
+example_bag.close()
